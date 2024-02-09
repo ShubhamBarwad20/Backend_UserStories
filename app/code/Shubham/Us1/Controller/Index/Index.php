e3c8214d67c2ea002722935269acf281159d9bf6
Shubham <shubham.barwad@hbwsl.com>
2024-02-09 17:13:15 +0530

    User Story 1

---
+++ app/code/Shubham/Us1/Controller/Index/Index.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types= 1);
+
+namespace Shubham\Us1\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\Result\JsonFactory;
+
+use Shubham\Us1\Test;
+
+class Index extends Action
+{
+    private $test;
+    private $resultJsonFactory;
+    public function __construct(Context $context, Test $test, JsonFactory $resultJsonFactory)
+    {
+        parent::__construct($context);
+        $this->test = $test;
+        $this->resultJsonFactory = $resultJsonFactory;
+    }
+
+    public function execute()
+    {
+        $result = $this->resultJsonFactory->create();
+        return $result->setData([$this->test->displayParams()]);
+    }
+
+}
---
+++ app/code/Shubham/Us1/Test.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Shubham\Us1;
+
+use Magento\Catalog\Api\Data\CategoryInterface;
+
+class Test
+{
+    protected $category;
+    protected $string;
+    protected $array;
+    public function __construct(CategoryInterface $category, $string='Shubham', $array=[1,2,3])
+    {
+        $this->category = $category;
+        $this->string = $string;
+        $this->array = $array;
+    }
+
+    public function displayParams()
+    {
+        echo "<p>";
+        print_r($this->array);
+        echo "</p>";
+        echo "String: ".$this->string;
+        return [$this->array, $this->string];
+    }
+}
