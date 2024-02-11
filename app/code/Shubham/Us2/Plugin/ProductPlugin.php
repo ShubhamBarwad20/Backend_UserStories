74ad09cc7718346d10095dceb6e5caeefb05fb13
Shubham <shubham.barwad@hbwsl.com>
2024-02-12 03:30:20 +0530

    User Story 2

--- app/code/Shubham/Us2/Plugin/ProductPlugin.php
+++ app/code/Shubham/Us2/Plugin/ProductPlugin.php
@@ -8,19 +8,25 @@ class ProductPlugin
 {
     public function afterGetName(\Magento\Catalog\Model\Product $subject, $result)
     {
+        $result .= ' On Sale!';
         if ($subject->getFinalPrice() < 60) {
-            $result .= ' On Sale!';
+            if($subject->getFinalPrice() < 20) {
+                $result .= 'WholeSale !! <strong>Discount %15</strong>';
+            }else if($subject->getFinalPrice() < 50) {
+                $result .= 'Super Sale!!';
+            }
         }
         return $result;
     }
 
     public function afterGetPrice(\Magento\Catalog\Model\Product $subject, $result)
     {
-        if($subject->getFinalPrice() < 50) {
+        // if($subject->getFinalPrice() < 50) {
             // if($subject->getFinalPrice() > 20){
                 $result = $result*0.85;
+                $result = $result + 1.79;
             // }
-        }
+        // }
         return $result;
     }
 }
